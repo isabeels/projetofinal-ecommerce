4cfbae2c33e351d0dc7b408b3970ec9a3011926a
RosanaDantas <rfdrosana@gmail.com>
2022-05-24 17:00:03 -0300

    Criação de anotações da classe funcionário

--- src/main/java/org/serratec/backend/projetofinalecommerce/controller/ClienteController.java
+++ src/main/java/org/serratec/backend/projetofinalecommerce/controller/ClienteController.java
@@ -1,5 +1,6 @@
 package org.serratec.backend.projetofinalecommerce.controller;
 
 public class ClienteController {
+	 
 
 }
--- src/main/java/org/serratec/backend/projetofinalecommerce/entity/Funcionario.java
+++ src/main/java/org/serratec/backend/projetofinalecommerce/entity/Funcionario.java
@@ -1,5 +1,64 @@
 package org.serratec.backend.projetofinalecommerce.entity;
 
-public class Funcionario {
+import java.sql.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "funcionario")
+public class Funcionario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "funcionario_cd_id")
+	private Integer idFuncionario;
+
+	@Column(name = "funcionario_tx_nomeCompleto")
+	private String nomeCompleto;
+
+	@Column(name = "funcionario_tx_cpf")
+	private Integer cpf;
+	
+	public Funcionario() {
+		
+	}
+
+	public Funcionario(Integer idFuncionario, String nomeCompleto, Integer cpf) {
+		super();
+		this.idFuncionario = idFuncionario;
+		this.nomeCompleto = nomeCompleto;
+		this.cpf = cpf;
+	}
+
+	public Integer getIdFuncionario() {
+		return idFuncionario;
+	}
+
+	public void setIdFuncionario(Integer idFuncionario) {
+		this.idFuncionario = idFuncionario;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public Integer getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(Integer cpf) {
+		this.cpf = cpf;
+	}
+
+	
 }
+

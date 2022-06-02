06853202085c541b91c8a443f8c57e4bae57c8c0
Vinícius Soares <vinivsp2304@hotmail.com>
2022-06-02 19:39:45 -0300

    teste

--- src/main/java/org/serratec/backend/projetofinalecommerce/entity/Categoria.java
+++ src/main/java/org/serratec/backend/projetofinalecommerce/entity/Categoria.java
@@ -16,7 +16,7 @@ public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "categoria_cd_id")
 	private Integer idCategoria;
 

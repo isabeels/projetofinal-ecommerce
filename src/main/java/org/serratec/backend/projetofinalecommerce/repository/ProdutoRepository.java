18f2b89f321a135d6376bdb3a76718217330bc14
Vinícius Soares <vinivsp2304@hotmail.com>
2022-06-02 21:28:30 -0300

    correção nos métodos da CategoriaService

--- src/main/java/org/serratec/backend/projetofinalecommerce/repository/ProdutoRepository.java
+++ src/main/java/org/serratec/backend/projetofinalecommerce/repository/ProdutoRepository.java
@@ -1,7 +1,5 @@
 package org.serratec.backend.projetofinalecommerce.repository;
 
-import java.util.UUID;
-
 import org.serratec.backend.projetofinalecommerce.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;

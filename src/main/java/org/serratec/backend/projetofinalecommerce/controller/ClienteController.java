185a5329fed26792bf18b7420742c5566e7f30a7
Gabriel <gabrieltsf10@gmail.com>
2022-06-03 14:55:45 -0300

    Listando os métodos e implementando primeiro retorno da classe clientecontroller

--- src/main/java/org/serratec/backend/projetofinalecommerce/controller/ClienteController.java
+++ src/main/java/org/serratec/backend/projetofinalecommerce/controller/ClienteController.java
@@ -1,12 +1,27 @@
 package org.serratec.backend.projetofinalecommerce.controller;
 
+import org.serratec.backend.projetofinalecommerce.dto.ClienteDTO;
+import org.serratec.backend.projetofinalecommerce.exceptions.ClienteException;
+import org.serratec.backend.projetofinalecommerce.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping ("/cliente")
 public class ClienteController {
-	//@GetMapping("/listarTodos")
+    @Autowired
+    ClienteService clienteService;
+
+
+	@GetMapping("/listarTodos")
+    public ResponseEntity<String> salvar(@RequestBody ClienteDTO clienteDTO) throws ClienteException {
+        return ResponseEntity.ok(clienteService.salvar(clienteDTO));
+    }
+
     //@GetMapping("/listar/{idCliente}")
     //@PostMapping("/")
     //@PostMapping("")

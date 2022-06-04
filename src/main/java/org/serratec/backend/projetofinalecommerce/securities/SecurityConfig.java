8ce0967455a7e5a477a8ed496e7bada05e463711
Vinícius Soares <vinivsp2304@hotmail.com>
2022-06-03 21:08:32 -0300

    criado: pacote "securities" e classe SecurityConfig
    adicionado: métodos na classe SecurityConfig

---
+++ src/main/java/org/serratec/backend/projetofinalecommerce/securities/SecurityConfig.java
@@ -0,0 +1,63 @@
+package org.serratec.backend.projetofinalecommerce.securities;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SuppressWarnings("deprecation")
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+		.inMemoryAuthentication()
+		.passwordEncoder(passwordEncoder())
+		.withUser("dev6")
+		.password(passwordEncoder().encode("admin"))
+		.roles("ADMIN");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.csrf().disable()
+		.authorizeRequests()
+		.antMatchers()
+		.permitAll()
+		.anyRequest()
+		.authenticated()
+		.and().sessionManagement();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+/*
+ 	nosso próprio sistema de encodificação
+ 	
+ 	
+	@Bean
+	public PasswordEncoder passwordEncoder () {
+		PasswordEncoder pEncoder = new PasswordEncoder() {
+			@Override
+			public String encode(CharSequence rawPassword) {
+				return rawPassword + "123";
+			}
+			
+			@Override
+			public boolean matches(CharSequence rawPassword, String encodedPassword) {
+				
+				return (rawPassword + "123").equals(encodedPassword);
+			}
+		}
+	} 
+*/
+
+}

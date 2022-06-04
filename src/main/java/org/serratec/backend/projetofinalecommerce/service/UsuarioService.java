d6b1fd1304509c8f71bcc0cd4f6102787ef2d824
Vinícius Soares <vinivsp2304@hotmail.com>
2022-06-04 13:09:19 -0300

    criado: UsuarioService / pacote service.
    Até o momento, a aplicação está subindo sem maiores problemas, porém,
    com o acesso ao localhost:8080 negado /403 unauthorized

--- src/main/java/org/serratec/backend/projetofinalecommerce/service/UsuarioService.java
+++ src/main/java/org/serratec/backend/projetofinalecommerce/service/UsuarioService.java
@@ -1,5 +1,7 @@
 package org.serratec.backend.projetofinalecommerce.service;
 
+import org.serratec.backend.projetofinalecommerce.dto.UsuarioDTO;
+import org.serratec.backend.projetofinalecommerce.entity.Usuario;
 import org.serratec.backend.projetofinalecommerce.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -7,12 +9,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
-	
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
+
 	@Autowired
 	private PasswordEncoder passwordEncoder;
+
+	private UsuarioDTO transformarEntityEmDto(Usuario usuario, UsuarioDTO usuarioDto) {
+		usuarioDto.setIdUsuario(usuario.getIdUsuario());
+		usuarioDto.setUsername(usuario.getUsername());
+		usuarioDto.setPassword(usuario.getPassword());
+		return usuarioDto;
+	}
 	
-	
+	private Usuario transformarDtoEmEntity (UsuarioDTO usuarioDto, Usuario usuario) {
+		usuario.setUsername(usuarioDto.getUsername());
+		usuario.setPassword(passwordEncoder.encode(usuarioDto.getPassword()));
+		return usuario;
+	}
 }

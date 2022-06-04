59bf6db9b289bcf3c0388c5edcaff91e2b4fcbfd
Vinícius Soares <vinivsp2304@hotmail.com>
2022-06-04 12:03:06 -0300

    criado: UsuarioRepository / pacote: repository

---
+++ src/main/java/org/serratec/backend/projetofinalecommerce/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package org.serratec.backend.projetofinalecommerce.repository;
+
+import org.serratec.backend.projetofinalecommerce.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
+
+}
--- src/main/java/org/serratec/backend/projetofinalecommerce/securities/SecurityConfig.java
+++ src/main/java/org/serratec/backend/projetofinalecommerce/securities/SecurityConfig.java
@@ -58,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			}
 		}
 	} 
+	
 */
 
 }

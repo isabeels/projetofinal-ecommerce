5988e6dac7181b9a5d95c3416364d24c81fb2a47
Vinícius Soares <vinivsp2304@hotmail.com>
2022-06-04 13:33:25 -0300

    implementado: método(s) de busca na classe UserServiceImpl / pacote service
    metódo(s) de busca na classe UsuarioRepository / pacote repository

--- src/main/java/org/serratec/backend/projetofinalecommerce/repository/UsuarioRepository.java
+++ src/main/java/org/serratec/backend/projetofinalecommerce/repository/UsuarioRepository.java
@@ -1,8 +1,14 @@
 package org.serratec.backend.projetofinalecommerce.repository;
 
+import java.util.Optional;
+
 import org.serratec.backend.projetofinalecommerce.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
-
+	
+	Optional<Usuario> buscarPorUsername(String username);
+		
 }

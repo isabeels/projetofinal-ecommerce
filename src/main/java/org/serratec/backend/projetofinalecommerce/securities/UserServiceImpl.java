4ba9c0b90d0f2ba1289c0de0907eba39202539bb
Vinícius Soares <vinivsp2304@hotmail.com>
2022-06-03 21:37:27 -0300

    Criado: classe UserServiceImpl / pacote securities

---
+++ src/main/java/org/serratec/backend/projetofinalecommerce/securities/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package org.serratec.backend.projetofinalecommerce.securities;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return null;
+	}
+
+}

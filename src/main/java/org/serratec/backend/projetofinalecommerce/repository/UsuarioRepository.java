598bda82ee023974ce9917a1fb98bf9717b66c86
Vinícius Soares <vinivsp2304@hotmail.com>
2022-06-04 13:34:17 -0300

    adicionado: @Querry em UsuarioRepository

--- src/main/java/org/serratec/backend/projetofinalecommerce/repository/UsuarioRepository.java
+++ src/main/java/org/serratec/backend/projetofinalecommerce/repository/UsuarioRepository.java
@@ -4,11 +4,13 @@ import java.util.Optional;
 
 import org.serratec.backend.projetofinalecommerce.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
 	
+	@Query(value="FROM Usuario u WHERE u.username = ?1")
 	Optional<Usuario> buscarPorUsername(String username);
 		
 }
